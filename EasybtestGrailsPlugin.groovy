d27ee97b34f1b46c3ca3c422f3b58adc39e10051
Rodrigo Urubatan Ferreira Jardim <rodrigo@urubatan.com.br>
2008-04-04 15:07:29 -0300

    first relase, need to write a post about ir now :D

--- EasybtestGrailsPlugin.groovy
+++ EasybtestGrailsPlugin.groovy
@@ -1,34 +1,53 @@
 
 class EasybtestGrailsPlugin {
-    def version = 0.1
-    def author = "Rodrigo Uruatan Ferreira Jardim"
-    def authorEmail = "blog@urubatan.com.br"
-    def title = "This plugin enables the testing of the application using the BDD easyb library"
-    def dependsOn = [:]
-	
-    def doWithSpring = {
-        // TODO Implement runtime spring config (optional)
-    }
-   
-    def doWithApplicationContext = { applicationContext ->
-        // TODO Implement post initialization spring config (optional)		
-    }
+  def version = 0.2;
+  def author = "Rodrigo Uruatan Ferreira Jardim";
+  def authorEmail = "blog@urubatan.com.br";
+  def title = "This plugin enables the testing of the application using the BDD easyb library";
+  def dependsOn = [:];
+  def description = '''\
+    This plugin enables the use of Behavior Driven Development to test your Grails Application,
+    The library behind this is easyb, easyb is a BDD library written in groovy.
+    For example, you can test your app like this:
 
-    def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional)
+    given "an invalid zip code", {
+      invalidzipcode = "221o1"
     }
-	                                      
-    def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
+    and "given the zipcodevalidator is initialized", {
+      zipvalidate = new ZipCodeValidator()
     }
-	
-    def onChange = { event ->
-        // TODO Implement code that is executed when this class plugin class is changed  
-        // the event contains: event.application and event.applicationContext objects
+    when "validate is invoked with the invalid zip code", {
+      value = zipvalidate.validate(invalidzipcode)
     }
-                                                                                  
-    def onApplicationChange = { event ->
-        // TODO Implement code that is executed when any class in a GrailsApplication changes
-        // the event contain: event.source, event.application and event.applicationContext objects
+    then "the validator instance should return false", {
+      value.shouldBe false
     }
+    '''
+  def documentation = "http://www.urubatan.info/tag/easyb-test/"
+
+  def doWithSpring = {
+  // TODO Implement runtime spring config (optional)
+  }
+
+  def doWithApplicationContext = { applicationContext ->
+  // TODO Implement post initialization spring config (optional)
+  }
+
+  def doWithWebDescriptor = { xml ->
+  // TODO Implement additions to web.xml (optional)
+  }
+
+  def doWithDynamicMethods = { ctx ->
+    // TODO Implement registering dynamic methods to classes (optional)
+  }
+
+  def onChange = { event ->
+  // TODO Implement code that is executed when this class plugin class is changed
+  // the event contains: event.application and event.applicationContext objects
+  }
+
+  def onApplicationChange = { event ->
+  // TODO Implement code that is executed when any class in a GrailsApplication changes
+  // the event contain: event.source, event.application and event.applicationContext objects
+  }
 }
