00974604dbe13e6b35aa48ff571ed9d6d3aa8a05
Rodrigo Urubatan Ferreira Jardim <rodrigo@urubatan.com.br>
2008-04-04 14:25:51 -0300

    there is still hope, running tests in the grails context now

--- test/behavior/TestStory.groovy
+++ test/behavior/TestStory.groovy
@@ -3,6 +3,6 @@ scenario "Any Test could be here", {
 	given "Any thing",{}
 	when "Some thing happens", {}
 	then "A test must run", {
-		def sf = ctx.getBean("sessionFactory")
+		def sf = appCtx.getBean("sessionFactory")
 	}
 }
